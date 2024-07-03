b840deb2bdacce784f52603a256e827874e7345e
Ignacio Esposto <ignaciospst@gmail.com>
2024-07-03 04:01:30 -0300

    feat!: playable game

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,13 @@
-const Config = {
+import Main from "../scenes/Main";
+import Board from "../scenes/Board";
+import GameOver from "../scenes/GameOver";
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 800,
-  height: 500,
+  height: 600,
+  backgroundColor: "#ffffff",
+  scene: [Main, Board, GameOver],
 };
 
-export default Config;
+export default gameConfig;
---
+++ src/scenes/Main.ts
@@ -0,0 +1,39 @@
+import Phaser from "phaser";
+import { initScene } from "../utils";
+
+export default class Main extends Phaser.Scene {
+  constructor() {
+    super("Main");
+  }
+  preload() {
+    this.load.image("Play", "assets/Play.png");
+  }
+  create() {
+    initScene(this);
+
+    this.add
+      .text(400, 100, "Tic Tac Toe", {
+        fontSize: "96px",
+        fontStyle: "600",
+        color: "black",
+      })
+      .setOrigin(0.5);
+
+    const playImg = this.add.image(400, 350, "Play").setScale(0.3);
+    playImg.setInteractive();
+
+    this.tweens.add({
+      targets: playImg,
+      scale: 0.4,
+      duration: 500,
+      ease: "Power1",
+      yoyo: true,
+      repeat: -1,
+    });
+    playImg.on("pointerdown", () => {
+      this.scene.stop("Main");
+      this.scene.start("Board");
+    });
+  }
+  update() {}
+}
